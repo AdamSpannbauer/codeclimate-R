60e0de6c37f5db03f01244524fdd8ba99472802b
Adam Spannbauer <SpannbauerAdam@gmail.com>
2018-10-25 09:28:50 -0700

    add test_assets dir for dev process

---
+++ test_assets/my_r_project.R
@@ -0,0 +1,9 @@
+# equals for assignment
+x = 1
+
+# no spaces around infix operator
+y<-2
+
+# line over 80 characters
+# variable name over 30 characters
+asdfasdfasdfasdfsadfasdfasdfsadfsadfsadfasdfasdfasdfsadfsadfsadfasdfsadfasdfsfsdaf <- x + y
